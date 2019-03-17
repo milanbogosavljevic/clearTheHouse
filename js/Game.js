8317b059b79de50ab5d34a3e3264dd76018ca813
Konan <andrej792885@gmail.com>
2019-03-17 20:51:24 +0100

    camera logic added

--- js/Game.js
+++ js/Game.js
@@ -12,16 +12,54 @@ this.system = this.system || {};
 
     p._player = null;
     p._playerMovement = null;
+    p._level = null;
+
+    p._CAMERA_WIDTH = null;
+    p._CAMERA_HEIGHT = null;
+
+    p._LEVEL_WIDTH = null;
+    p._LEVEL_HEIGHT = null;
+
+    p._PLAYER_LEFT_BORDER = null;
+    p._PLAYER_RIGHT_BORDER = null;
+    p._PLAYER_UP_BORDER = null;
+    p._PLAYER_DOWN_BORDER = null;
+
+    p._LEFT_AREA_BORDER = null;
+    p._RIGHT_AREA_BORDER = null;
+    p._UP_AREA_BORDER = null;
+    p._DOWN_AREA_BORDER = null;
 
     p._init = function () {
+        this._CAMERA_WIDTH = stage.canvas.width;
+        this._CAMERA_HEIGHT = stage.canvas.height;
+
+        this._level = new createjs.Container();
+
         const back = system.CustomMethods.makeImage('background', false);
-        this.addChild(back);
+        this._level.addChild(back);
+
+        this._LEVEL_WIDTH = back.image.width;
+        this._LEVEL_HEIGHT = back.image.height;
+
+        this._LEFT_AREA_BORDER = this._CAMERA_WIDTH/2;
+        this._RIGHT_AREA_BORDER = this._LEVEL_WIDTH - (this._LEVEL_WIDTH/4);
+        this._UP_AREA_BORDER = this._CAMERA_HEIGHT/2;
+        this._DOWN_AREA_BORDER = this._LEVEL_HEIGHT - (this._LEVEL_HEIGHT/4);
 
         const player = this._player = new system.Player();
         player.x = 300;
         player.y = 500;
 
-        this.addChild(player);
+        const playerDimension = player.getDimension();
+        this._PLAYER_LEFT_BORDER = playerDimension.width/2;
+        this._PLAYER_RIGHT_BORDER = this._LEVEL_WIDTH - (playerDimension.width/2);
+        this._PLAYER_UP_BORDER = playerDimension.height/2;
+        this._PLAYER_DOWN_BORDER = this._LEVEL_HEIGHT - (playerDimension.height/2);
+
+        this._level.addChild(player);
+
+        this.addChild(this._level);
 
         this._setPlayerMovement();
 
@@ -35,22 +73,22 @@ this.system = this.system || {};
     };
 
     p._handleKey = function(key, bool) {
-        if(key === 'a'){
+        if(key === 'a' || key === 'A'){
             if(this._playerMovement.left !== bool){
                 this._playerMovement.left = bool;
             }
         }
-        if(key === 'd'){
+        if(key === 'd' || key === 'D'){
             if(this._playerMovement.right !== bool){
                 this._playerMovement.right = bool;
             }
         }
-        if(key === 'w'){
+        if(key === 'w' || key === 'W'){
             if(this._playerMovement.up !== bool){
                 this._playerMovement.up = bool;
             }
         }
-        if(key === 's'){
+        if(key === 's' || key === 'S'){
             if(this._playerMovement.down !== bool){
                 this._playerMovement.down = bool;
             }
@@ -68,16 +106,55 @@ this.system = this.system || {};
 
     p._movePlayer = function() {
         if(this._playerMovement.left === true){
-            this._player.x -= this._player.speed;
+            if(this._player.x > this._PLAYER_LEFT_BORDER){
+                this._player.x -= this._player.speed;
+            }
         }
         if(this._playerMovement.right === true){
-            this._player.x += this._player.speed;
+            if(this._player.x < this._PLAYER_RIGHT_BORDER) {
+                this._player.x += this._player.speed;
+            }
         }
         if(this._playerMovement.up === true){
-            this._player.y -= this._player.speed;
+            if(this._player.y > this._PLAYER_UP_BORDER){
+                this._player.y -= this._player.speed;
+            }
         }
         if(this._playerMovement.down === true){
-            this._player.y += this._player.speed;
+            if(this._player.y < this._PLAYER_DOWN_BORDER){
+                this._player.y += this._player.speed;
+            }
+        }
+    };
+
+    p._moveLevel = function() {
+        if(this._playerMovement.right === true){
+            if(this._player.x > this._LEFT_AREA_BORDER){
+                if(this._level.x > -(this._CAMERA_WIDTH)){
+                    this._level.x -= this._player.speed;
+                }
+            }
+        }
+        if(this._playerMovement.left === true){
+            if(this._player.x < this._RIGHT_AREA_BORDER){
+                if(this._level.x < 0) {
+                    this._level.x += this._player.speed;
+                }
+            }
+        }
+        if(this._playerMovement.down === true){
+            if(this._player.y > this._UP_AREA_BORDER){
+                if(this._level.y > -(this._CAMERA_HEIGHT)) {
+                    this._level.y -= this._player.speed;
+                }
+            }
+        }
+        if(this._playerMovement.up === true){
+            if(this._player.y < this._DOWN_AREA_BORDER){
+                if(this._level.y < 0) {
+                    this._level.y += this._player.speed;
+                }
+            }
         }
     };
 
@@ -88,6 +165,7 @@ this.system = this.system || {};
     p.render = function(e){
         stage.update(e);
         this._movePlayer();
+        this._moveLevel();
     };
 
     system.Game = createjs.promote(Game,"Container");
--- js/Player.js
+++ js/Player.js
@@ -11,12 +11,23 @@ this.system = this.system || {};
     const p = createjs.extend(Player,createjs.Container);
 
     p.speed = null;
+    p._width = null;
+    p._height = null;
 
     p._init = function () {
         const body = system.CustomMethods.makeImage('player', false, true);
         this.addChild(body);
 
         this.speed = 10;
+        this._width = body.image.width;
+        this._height = body.image.height;
+    };
+
+    p.getDimension = function() {
+        return {
+            width:this._width,
+            height:this._height
+        }
     };
 
     system.Player = createjs.promote(Player,"Container");
